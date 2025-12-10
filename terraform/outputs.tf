c5b278e702babdcbe9b7aec29d9287d8e60c5746
Anandha Padmanaban J C <34396688+anandha-jc@users.noreply.github.com>
2025-12-10 00:35:37 +0000

    Mark static_web_app_api_key output as sensitive

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,7 +11,7 @@ output "static_web_app_default_hostname" {
 output "static_web_app_api_key" {
   description = "The API key for deploying to the Static Web App"
   value       = azurerm_static_web_app.main.api_key
-# sensitive   = true
+  sensitive   = true
 }
 
 output "resource_group_name" {
