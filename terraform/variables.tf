7b233d9ec0357884bd7b05142c8f4126b7c2feec
Anandha Padmanaban J C <34396688+anandha-jc@users.noreply.github.com>
2025-12-10 00:09:51 +0000

    Change default location to 'uksouth'

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "Azure region for resources"
   type        = string
-  default     = "eastus"
+  default     = "uksouth"
 }
 
 variable "static_web_app_name" {
